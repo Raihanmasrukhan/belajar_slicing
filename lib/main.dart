664a71fb97364ed1b3369663675bdb1ade2ad35b
Raihan <raihanmasrukhan56@gmail.com>
2025-03-06 22:37:38 +0700

    HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:coba_flutter/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:coba_flutter/login.dart';
 import 'package:coba_flutter/splash.dart';
+import 'package:coba_flutter/homePage.dart';
+import 'package:coba_flutter/praktek.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Baru'),
       debugShowCheckedModeBanner: false,
-      home: MySplash(),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/praktek.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Map<String, dynamic>> tickets = [
+    {
+      'id': 1,
+      'type': 'Tiket Masuk Dewasa',
+      'category': 'Nusantara',
+      'price': 50.000,
+      'quantity': 2
+    },
+    {
+      'id': 2,
+      'type': 'Tiker Masuk Anak',
+      'category': "Nusantara",
+      'price': 20.000,
+      'quantity': 2
+    },
+    {
+      'id': 3,
+      'type': 'Tiket Masuk Dewasa',
+      'category': 'Mancanegara',
+      'price': 150.000,
+      'quantity': 0
+    },
+    {
+      'id': 4,
+      'type': 'Tiket Masuk Anak',
+      'category': 'Mancanegara',
+      'price': 40.000,
+      'quantity': 0
+    }
+  ];
+
+  void updateQuantity(int id, int amount) {
+    setState(() {
+      var ticket = tickets.firstWhere((ticket) => ticket['id'] == id);
+      ticket['quantity'] = (ticket['quantity'] + amount).clamp(0, 99);
+    });
+  }
+
+  int get totalPrice => tickets.fold(
+      0,
+      (sum, ticket) =>
+          sum + ((ticket['quantity'] as int) * (ticket['price'] as int)));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Penjualan',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: tickets.length,
+                itemBuilder: (context, index) {
+                  final ticket = tickets[index];
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            ticket['type'],
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                            ticket['category'],
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                          Text(
+                            'Rp. ${ticket['price']}',
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.remove,
+                                    color: Colors.black),
+                                onPressed: () =>
+                                    updateQuantity(ticket['id'], -1),
+                              ),
+                              Text(
+                                ticket['quantity'].toString(),
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.add,
+                                  color: Colors.blue,
+                                ),
+                                onPressed: () =>
+                                    updateQuantity(ticket['id'], 1),
+                              ),
+                            ],
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              'Rp. ${ticket['quantity'] * ticket['price']}',
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Order Summary',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Rp. $totalPrice',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                ),
+                child: const Text(
+                  'Process',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
